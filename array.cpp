dbcfa0cf67563a4a784fb2dc36abaf1ab72de9f9
1ARZAMAS <lizaakutkina@gmail.com>
2024-09-30 20:19:36 +0700

    added cpp file

---
+++ array.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node(){
+        Node* nextNode = nullptr;
+    }
+};
+struct Array(){
+    Node* head;
+    int size;
+    void add();
+    void addToTheEnd();
+    void get();
+    void remove();
+    void replace();
+    void length();
+    void display();
+};
+
+void Array::add(){
+
+}
+
+void Array::addToTheEnd(){
+    
+}
+
+void Array::get(){
+    
+}
+
+void Array::remove(){
+    
+}
+
+void Array::replace(){
+    
+}
+
+void Array::length(){
+    
+}
+
+void Array::display(){
+    
+}
+
+int main(){
+
+}
