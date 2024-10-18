e289bfc6dbd62b42bb5fae2cb66605acf1e9c6ce
1ARZAMAS <lizaakutkina@gmail.com>
2024-10-18 23:38:22 +0700

    removed unnecessary lines

--- main.cpp
+++ main.cpp
@@ -2,17 +2,6 @@
 
 int main(int argc, char* argv[]) {
     std::string filename;
-
-    // Обработка аргументов командной строки
-    for (int i = 1; i < argc; i++) {
-        string arg = argv[i];
-        if (arg == "--file") {
-            if (i + 1 < argc) {
-                filename = argv[++i];
-            }
-        }
-    }
-
     // Интерактивный ввод команд
     std::string query;
     cout << "Enter the command (ot 'exit' to exit):" << endl;
